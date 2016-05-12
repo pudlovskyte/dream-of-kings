00d32c728f1e5c51ea88a2eb1da210b0fe784041
Monika Pudlovskyte <monika@pudlovskyte.lt>
2016-05-12 21:46:37 +0200

    Update js

--- js/menu.js
+++ js/menu.js
@@ -13,38 +13,37 @@ var isLinkToMenu = function(el){
 		function(elem){ return elem.getAttribute('href') === '#menu'; });
 };
 
-var menu = document.getElementById('menu');
-
-var showMenu = function(){
-	menu.style.display = 'flex';
-	menu.focus();
-};
-
-var hideMenu = function(){
-	menu.style.display = 'none';
-};
-
-document.querySelector('[href="#menu"]').addEventListener('click', showMenu);
-
-document.body.addEventListener('click', function(e){
-	if (isElementInsideMenu(e.target) || isLinkToMenu(e.target))
-		return;
-	hideMenu();
-});
-
-menu.addEventListener('click', function(e){
-	if (e.target.getAttribute('href'))
-		hideMenu();
-});
-
 var isEventKeyEscape = function(e){
 	if (e.key)
 		return e.key === 'Escape';
 	return e.which === 27;
 };
 
-document.body.addEventListener('keyup', function(e){
-	if (isEventKeyEscape(e))
-		hideMenu();
-});
+var menu = document.getElementById('menu');
 
+if (menu){
+	var showMenu = function(){
+		menu.style.display = 'flex';
+		menu.focus();
+	};
+
+	var hideMenu = function(){
+		menu.style.display = 'none';
+	};
+
+	document.querySelector('[href="#menu"]').addEventListener('click', showMenu);
+	document.body.addEventListener('click', function(e){
+		if (isElementInsideMenu(e.target) || isLinkToMenu(e.target))
+			return;
+		hideMenu();
+	});
+
+	menu.addEventListener('click', function(e){
+		if (e.target.getAttribute('href'))
+			hideMenu();
+	});
+	document.body.addEventListener('keyup', function(e){
+		if (isEventKeyEscape(e))
+			hideMenu();
+	});
+}
